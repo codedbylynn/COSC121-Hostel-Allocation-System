0284f94c5b6aa33e8067efc24672511c3bfa38eb
codedbylynn <lynngathoni9@gmail.com>
2026-04-05 16:32:51 +0300

    Add main.cpp for Smart Hostel Room Allocation project
    Initial implementation of the Smart Hostel Room Allocation project with basic output.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace .std;
+/*================================================================================================================
+  cosc 121-OOP Group Project
+Project Title: Smart Hostel Room Allocation
+Group Members:
+1. Lynn Gathoni
+2. Humphrey Meshack
+3. Issac Mwangi
+4. Martin kilonzo
+5. Lyn Ntinyari
+
+int main(){
+  cout<<"Smart Hostel Room Allocation System"<<endl;
+return 0;
+}
